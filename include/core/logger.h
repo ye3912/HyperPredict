a30672569377061f71153b0d84ea6d14fbe6dbb4
ye3912 <2966443912@qq.com>
2026-04-13 00:42:58 +0800

    Update logger.h

--- include/core/logger.h
+++ include/core/logger.h
@@ -1,13 +1,23 @@
+cat > include/core/logger.h << 'EOF'
 #pragma once
 #include <android/log.h>
 
 namespace hp {
-enum class LogLevel : int { DEBUG=3, INFO=4, WARN=5, ERROR=6 };
-void init(const char* tag, LogLevel level);
+
+enum class LogLevel : int { 
+    DEBUG = ANDROID_LOG_DEBUG, 
+    INFO = ANDROID_LOG_INFO, 
+    WARN = ANDROID_LOG_WARN, 
+    ERROR = ANDROID_LOG_ERROR 
+};
+
+void init_logger(const char* tag, LogLevel level);
 void log(LogLevel lvl, const char* fmt, ...);
-}
+
+} // namespace hp
 
 #define LOGD(...) hp::log(hp::LogLevel::DEBUG, __VA_ARGS__)
 #define LOGI(...) hp::log(hp::LogLevel::INFO, __VA_ARGS__)
 #define LOGW(...) hp::log(hp::LogLevel::WARN, __VA_ARGS__)
 #define LOGE(...) hp::log(hp::LogLevel::ERROR, __VA_ARGS__)
+EOF

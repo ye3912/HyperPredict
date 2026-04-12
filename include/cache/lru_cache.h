61915d475aecdcbac361b7dbd7aeb1a07a9d3055
ye3912 <2966443912@qq.com>
2026-04-13 00:27:24 +0800

    Refactor LRUCache to use CacheEntry structure

--- include/cache/lru_cache.h
+++ include/cache/lru_cache.h
@@ -6,63 +6,92 @@
 #include "core/types.h"
 
 namespace hp::cache {
+
 struct Key {
-    char pkg[64], scene[32];
+    char pkg[64];
+    char scene[32];
+    
     bool operator==(const Key& o) const {
-        return std::string_view(pkg) == o.pkg && std::string_view(scene) == o.scene;
+        return std::string_view(pkg) == o.pkg && 
+               std::string_view(scene) == o.scene;
     }
 };
+
 struct Hash {
     size_t operator()(const Key& k) const noexcept {
         size_t h = 14695981039346656037ull;
-        for(char c : k.pkg) { h ^= c; h *= 1099511628211ull; }
-        for(char c : k.scene) { h ^= c; h *= 1099511628211ull; }
+        for (char c : k.pkg) { h ^= c; h *= 1099511628211ull; }
+        for (char c : k.scene) { h ^= c; h *= 1099511628211ull; }
         return h;
     }
 };
 
 template<size_t Segs = 16>
 class LRUCache {
-    struct alignas(64) Seg {
+    struct CacheEntry {
+        FreqConfig config;
+        std::list<Key>::iterator lru_it;
+    };
+
+    struct alignas(64) Segment {
         mutable std::shared_mutex mtx;
         std::list<Key> lru;
-        std::unordered_map<Key, FreqConfig, Hash> map;
+        std::unordered_map<Key, CacheEntry, Hash> map;
     };
-    std::array<Seg, Segs> segs_;
-    size_t max_per_;
-    static size_t h(const Key& k) noexcept { return Hash{}(k) % Segs; }
+
+    std::array<Segment, Segs> segments_;
+    size_t max_per_segment_;
+
+    static size_t segment_index(const Key& k) noexcept {
+        return Hash{}(k) % Segs;
+    }
 
 public:
-    explicit LRUCache(size_t total_max = 8192) : max_per_(total_max / Segs) {}
-    
+    explicit LRUCache(size_t total_max = 8192) 
+        : max_per_segment_(total_max / Segs) {}
+
     std::optional<FreqConfig> get(const Key& k) noexcept {
-        auto& s = segs_[h(k)];
-        std::shared_lock lk(s.mtx);
-        auto it = s.map.find(k);
-        if(it == s.map.end()) return std::nullopt;
+        auto& seg = segments_[segment_index(k)];
+        std::shared_lock lk(seg.mtx);
+        
+        auto it = seg.map.find(k);
+        if (it == seg.map.end()) {
+            return std::nullopt;
+        }
+
+        // Move to front of LRU
         lk.unlock();
-        std::unique_lock ul(s.mtx);
-        s.lru.erase(it->second.second);
-        s.lru.push_front(k);
-        it->second.second = s.lru.begin();
-        return it->second;
+        std::unique_lock ulk(seg.mtx);
+        seg.lru.erase(it->second.lru_it);
+        seg.lru.push_front(k);
+        it->second.lru_it = seg.lru.begin();
+        
+        return it->second.config;
     }
 
-    void put(const Key& k, FreqConfig v) noexcept {
-        auto& s = segs_[h(k)];
-        std::unique_lock lk(s.mtx);
-        auto it = s.map.find(k);
-        if(it != s.map.end()) {
-            s.lru.erase(it->second.second);
-            it->second = v;
+    void put(const Key& k, FreqConfig cfg) noexcept {
+        auto& seg = segments_[segment_index(k)];
+        std::unique_lock lk(seg.mtx);
+
+        auto it = seg.map.find(k);
+        if (it != seg.map.end()) {
+            // Update existing
+            seg.lru.erase(it->second.lru_it);
+            it->second.config = cfg;
         } else {
-            if(s.map.size() >= max_per_) {
-                s.map.erase(s.lru.back());
-                s.lru.pop_back();
+            // Insert new
+            if (seg.map.size() >= max_per_segment_) {
+                // Evict LRU
+                const Key& evict_key = seg.lru.back();
+                seg.map.erase(evict_key);
+                seg.lru.pop_back();
             }
-            s.lru.push_front(k);
-            s.map.emplace(k, std::pair{v, s.lru.begin()});
+            seg.map.emplace(k, CacheEntry{cfg, seg.lru.end()});
         }
+        
+        seg.lru.push_front(k);
+        seg.map[k].lru_it = seg.lru.begin();
     }
 };
+
 } // namespace hp::cache

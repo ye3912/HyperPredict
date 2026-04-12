12faa7fb4f7dcdbc726e441f4a6878821c8f7e78
ye3912 <2966443912@qq.com>
2026-04-13 00:41:41 +0800

    Refactor logger initialization and monitor thread logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+cat > src/main.cpp << 'EOF'
 #include <csignal>
 #include <unistd.h>
 #include "core/event_loop.h"
@@ -9,17 +10,24 @@ void sig_handler(int) { g_run = 0; }
 int main() {
     signal(SIGTERM, sig_handler);
     signal(SIGINT, sig_handler);
-    hp::Logger::init("HyperPredict", hp::LogLevel::INFO);
+    
+    hp::init_logger("HyperPredict", hp::LogLevel::INFO);
     LOGI("HyperPredict v2.0.0 [DEVICE-ADAPTED + POWER-MODEL]");
     
     hp::EventLoop loop;
+    
     std::thread monitor([&]() {
-        while(g_run && loop.run_.load()) sleep(2);
+        while(g_run) {
+            if (!loop.is_running()) break;
+            sleep(2);
+        }
         loop.stop();
     });
     
     loop.start();
     monitor.join();
+    
     LOGI("Shutdown complete.");
     return 0;
 }
+EOF

5c10a2e47a05da03990ed33850f1fdbc7f4dadcb
ye3912 <2966443912@qq.com>
2026-04-14 12:30:22 +0800

    更新 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,22 @@
 #include <csignal>
 #include <unistd.h>
-#include <atomic>
 #include "core/event_loop.h"
 #include "core/logger.h"
 
-std::atomic<bool> g_run{true};
-
-void sig_handler(int sig) {
-    if (sig == SIGTERM || sig == SIGINT) {
-        g_run.store(false);
-    }
-}
+volatile sig_atomic_t g_run = 1;
+void sig_handler(int) { g_run = 0; }
 
 int main() {
     signal(SIGTERM, sig_handler);
     signal(SIGINT, sig_handler);
+    signal(SIGHUP, sig_handler);
     signal(SIGPIPE, SIG_IGN);
     
     hp::init_logger("HyperPredict", hp::LogLevel::INFO);
     LOGI("HyperPredict v2.0.0 [DEVICE-ADAPTED + POWER-MODEL]");
     
     hp::EventLoop loop;
-    
-    std::thread monitor([&]() {
-        while(g_run.load()) {
-            if (!loop.is_running()) break;
-            sleep(2);
-        }
-        loop.stop();
-    });
-    
     loop.start();
-    monitor.join();
     
     LOGI("Shutdown complete.");
     return 0;

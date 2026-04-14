fb77a9ae3197063b85ff70b36191964562724643
ye3912 <2966443912@qq.com>
2026-04-14 12:06:18 +0800

    更新 system_collector.h

--- include/core/system_collector.h
+++ include/core/system_collector.h
@@ -1,31 +1,23 @@
 #pragma once
-#include <cstdint>
-#include <string>
-#include <array>
 #include "core/types.h"
+#include <string>
 
 namespace hp {
 
 class SystemCollector {
-    struct CpuStat {
-        uint64_t user, nice, system, idle, iowait, irq, softirq;
-    };
-    std::array<CpuStat, 8> prev_stat_{};
-    std::array<uint32_t, 8> util_{};
-    uint32_t last_rq_{0}, last_wake_{0};
-    uint64_t last_frame_ts_{0};
-    uint32_t touch_count_{0};
-    uint64_t touch_ts_{0};
-    
-    bool read_cpu_stat();
-    bool read_run_queue();
-    bool read_thermal();
-    bool read_battery();
+    std::string proc_stat_path_;
+    std::string thermal_base_path_;
     
 public:
-    SystemCollector() noexcept;
+    SystemCollector();
     LoadFeature collect() noexcept;
-    uint32_t get_cpu_util(int cpu) const noexcept { return util_[cpu]; }
+    
+private:
+    uint32_t read_cpu_util() noexcept;
+    uint32_t read_run_queue() noexcept;
+    uint32_t read_wakeups() noexcept;
+    int8_t read_thermal_margin() noexcept;
+    uint8_t read_battery_level() noexcept;
 };
 
 } // namespace hp

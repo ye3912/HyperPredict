e92d0b0070cf856c2593693713230675db68e2ef
ye3912 <2966443912@qq.com>
2026-04-14 12:22:04 +0800

    更新 boot_calibrator.cpp

--- src/core/boot_calibrator.cpp
+++ src/core/boot_calibrator.cpp
@@ -1,54 +1,51 @@
 #include "core/boot_calibrator.h"
-#include "device/hardware.h"
-#include <cmath>
+#include "device/cpu_topology.h"
 #include <algorithm>
 
 namespace hp {
 
-void BootCalibrator::quick_freq_sweep() noexcept {
-    FreqKHz freqs[] = BASELINE_FREQS;
-    double p1 = 0.8, p2 = 2.1, p3 = 4.5;
-    baseline_.pwr = {
-        (p3 - p1) / (std::pow(static_cast<double>(freqs[12]), 3) - 
-                     std::pow(static_cast<double>(freqs[2]), 3)),
-        0.004,
-        (p1 - baseline_.pwr.a * std::pow(static_cast<double>(freqs[2]), 3)) / 
-         static_cast<double>(freqs[2])
-    };
-}
+void BootCalibrator::calibrate(const device::CpuTopology& topo) noexcept {
+    const auto& big_cores = topo.get_big_cores();
+    const auto& little_cores = topo.get_little_cores();
+    const auto& all_cores = topo.get_all_cores();
 
-BaselinePolicy BootCalibrator::calibrate() noexcept {
-    quick_freq_sweep();
+    if(all_cores.empty()) return;
+
+    uint32_t max_big_freq = 0;
+    for(int id : big_cores) {
+        for(const auto& c : all_cores) {
+            if(c.id == id) max_big_freq = std::max(max_big_freq, c.max_freq);
+        }
+    }
+    if(max_big_freq == 0) max_big_freq = 2600000;
+
+    uint32_t target_big = max_big_freq * 85 / 100;
+    uint32_t min_big = max_big_freq * 50 / 100;
+
+    uint8_t big_mask = 0;
+    for(int id : big_cores) {
+        if(id < 8) big_mask |= (1 << id);
+    }
     
-    baseline_.little.min_freq = 600000;
-    baseline_.little.target_freq = 1200000;
-    baseline_.little.boost_duration_ms = 150;
-    baseline_.little.cpuset_mask = 0x0F;
-    baseline_.little.uclamp_min = 10;
-    baseline_.little.uclamp_max = 40;
-    baseline_.little.ramp_ms = 30;
-    baseline_.little.config_hash = 0x0F;
-    
-    baseline_.mid.min_freq = 1200000;
-    baseline_.mid.target_freq = 1800000;
-    baseline_.mid.boost_duration_ms = 180;
-    baseline_.mid.cpuset_mask = 0x30;
-    baseline_.mid.uclamp_min = 30;
-    baseline_.mid.uclamp_max = 70;
-    baseline_.mid.ramp_ms = 25;
-    baseline_.mid.config_hash = 0x30;
-    
-    baseline_.big.min_freq = 1800000;
-    baseline_.big.target_freq = 2600000;
-    baseline_.big.boost_duration_ms = 220;
-    baseline_.big.cpuset_mask = 0xC0;
+    uint8_t little_mask = 0;
+    for(int id : little_cores) {
+        if(id < 8) little_mask |= (1 << id);
+    }
+    if(little_mask == 0) little_mask = ~big_mask;
+
+    baseline_.big.target_freq = target_big;
+    baseline_.big.min_freq = min_big;
+    baseline_.big.cpuset_mask = big_mask;
     baseline_.big.uclamp_min = 50;
     baseline_.big.uclamp_max = 100;
     baseline_.big.ramp_ms = 15;
-    baseline_.big.config_hash = 0xC0;
-    
-    baseline_.global_uclamp_max = 85;
-    return baseline_;
+
+    baseline_.little.target_freq = 1000000;
+    baseline_.little.min_freq = 600000;
+    baseline_.little.cpuset_mask = little_mask;
+    baseline_.little.uclamp_min = 10;
+    baseline_.little.uclamp_max = 40;
+    baseline_.little.ramp_ms = 30;
 }
 
 } // namespace hp

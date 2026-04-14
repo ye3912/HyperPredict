1a35b69b9c00290a773e218f78c36242f538095c
ye3912 <2966443912@qq.com>
2026-04-14 23:55:51 +0800

    更新 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 std::atomic<bool> g_run{true};
 
 void sig_handler(int sig) {
-    __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Signal %d received", sig);
+    __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Signal %d", sig);
     g_run.store(false, std::memory_order_release);
 }
 
@@ -25,17 +25,13 @@ int main() {
     sigaction(SIGINT, &sa, nullptr);
     signal(SIGPIPE, SIG_IGN);
     
-    __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Initializing event loop...");
+    __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Initializing...");
     
-    try {
-        hp::EventLoop loop;
-        __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Starting event loop...");
-        loop.start();
-        __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Event loop stopped");
-    } catch (const std::exception& e) {
-        __android_log_print(ANDROID_LOG_ERROR, "HyperPredict", "Fatal error: %s", e.what());
-        return 1;
-    }
+    hp::EventLoop loop;
+    
+    __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Starting...");
+    
+    loop.start();
     
     __android_log_print(ANDROID_LOG_INFO, "HyperPredict", "Shutdown complete");
     return 0;

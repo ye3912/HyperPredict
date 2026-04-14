f60c3f39398922767c1e762a0eae0c15c7a484a9
ye3912 <2966443912@qq.com>
2026-04-14 23:52:12 +0800

    更新 logger.cpp

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -1,19 +1,23 @@
 #include "core/logger.h"
+#include <android/log.h>
 #include <cstdarg>
 #include <cstdio>
 
 namespace hp {
 
 static const char* s_tag = "HyperPredict";
-static LogLevel s_level = LogLevel::INFO;
+static LogLevel s_level = LogLevel::DEBUG;
 
 void init_logger(const char* tag, LogLevel level) {
     s_tag = tag;
     s_level = level;
+    __android_log_print(ANDROID_LOG_DEBUG, s_tag, "Logger initialized (level=%d)", static_cast<int>(s_level));
 }
 
 void log(LogLevel lvl, const char* fmt, ...) {
-    if (static_cast<int>(lvl) < static_cast<int>(s_level)) return;
+    if (static_cast<int>(lvl) < static_cast<int>(s_level)) {
+        return;
+    }
     
     va_list args;
     va_start(args, fmt);

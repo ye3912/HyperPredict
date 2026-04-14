d5f8290fd3921825e45e0f6114eadb523fabb70f
ye3912 <2966443912@qq.com>
2026-04-14 12:07:52 +0800

    更新 feature_extractor.h

--- include/predict/feature_extractor.h
+++ include/predict/feature_extractor.h
@@ -3,6 +3,7 @@
 #include "core/types.h"
 
 namespace hp::predict {
+
 class FeatureExtractor {
     uint32_t ewma100_{0}, ewma500_{0}, last_{0};
     int32_t slope50_{0};
@@ -11,4 +12,5 @@ public:
                         uint32_t frame, uint32_t touch, int8_t therm, uint8_t bat) noexcept;
     std::array<float, 10> to_vec(const LoadFeature& f) const noexcept;
 };
+
 } // namespace hp::predict

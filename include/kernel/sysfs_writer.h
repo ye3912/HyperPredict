6e6ab7b1aeb63a6ed28b516a6a111a2e301fa05c
ye3912 <2966443912@qq.com>
2026-04-14 12:10:11 +0800

    更新 sysfs_writer.h

--- include/kernel/sysfs_writer.h
+++ include/kernel/sysfs_writer.h
@@ -4,12 +4,15 @@
 #include "core/types.h"
 
 namespace hp::kernel {
+
 class SysfsWriter {
     struct Cpu { int min_fd = -1, max_fd = -1; char buf[32]; };
-    std::array<Cpu, 8> cpus_;
+    std::array<Cpu, 8> cpus_{};
+    
 public:
     SysfsWriter() noexcept;
     ~SysfsWriter() noexcept;
     bool set_batch(const std::vector<std::pair<int, FreqConfig>>& cfgs) noexcept;
 };
+
 } // namespace hp::kernel

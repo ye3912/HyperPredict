c4ae9f14ce128631825b9df544475a95ce734db9
ye3912 <2966443912@qq.com>
2026-04-15 15:50:41 +0800

    更新 cpu_topology.h

--- include/device/cpu_topology.h
+++ include/device/cpu_topology.h
@@ -1,34 +1,24 @@
 #pragma once
 #include <vector>
-#include <string>
 #include <cstdint>
 
 namespace hp::device {
 
-struct CPUCore {
-    int id;
-    uint32_t max_freq;
-    uint32_t min_freq;
-    bool is_online;
-};
-
 class CpuTopology {
-    std::vector<CPUCore> cores_;
-    std::vector<int> big_cores_;
-    std::vector<int> little_cores_;
-    int highest_freq_core_{-1};
-    
-    uint32_t read_max_freq(int cpu) noexcept;
-    bool is_cpu_online(int cpu) noexcept;
-    void classify_cores() noexcept;
-    
 public:
+    struct Domain {
+        std::vector<int> cpus;
+        uint32_t min_freq{0};
+        uint32_t max_freq{0};
+    };
+
     bool detect() noexcept;
-    const std::vector<int>& get_big_cores() const noexcept { return big_cores_; }
-    const std::vector<int>& get_little_cores() const noexcept { return little_cores_; }
-    int get_highest_freq_core() const noexcept { return highest_freq_core_; }
-    const std::vector<CPUCore>& get_all_cores() const noexcept { return cores_; }
-    void print_info() const noexcept;
+    const std::vector<Domain>& get_domains() const noexcept { return domains_; }
+    int get_total_cpus() const noexcept { return total_cpus_; }
+
+private:
+    std::vector<Domain> domains_;
+    int total_cpus_{0};
 };
 
 } // namespace hp::device
